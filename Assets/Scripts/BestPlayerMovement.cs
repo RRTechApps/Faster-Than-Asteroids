ed3e14699d2c390a4b1a913417697fde5fd2cb44
Zach Goldberg <highlowplays@gmail.com>
2016-10-12 16:13:56 -0500

    new bug
    fixed 2 bugs added 1...

--- Assets/Scripts/BestPlayerMovement.cs
+++ Assets/Scripts/BestPlayerMovement.cs
@@ -15,13 +15,19 @@ public class BestPlayerMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		float rot = Input.GetAxis("Horizontal");
 		float vSped = Input.GetAxis("Vertical");
-		rb.transform.Rotate(new Vector3(0.0f, Input.GetAxis("Horizontal"), 0.0f));
+		rb.rotation = new Quaternion(1.0f,rot,1.0f,1.0f);
 		rb.AddRelativeForce(new Vector3(0.0f, 0.0f, vSped * speed));
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			waitTime(3);
+			rb.rotation = new Quaternion(1.0f,rot,1.0f,1.0f);
+		}
 	}
-
+	//Stabilization Func!
 	public IEnumerator waitTime(float waits){
-		rb.transform.Rotate(new Quaternion (Quaternion.identity));
+		transform.rotation = Quaternion.identity;
 		yield return new WaitForSeconds(waits);
 	}
 }
