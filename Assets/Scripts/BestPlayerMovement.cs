698cf4203486a2583dc94bd26d309af1c0dff352
20juanpls <shortwavepeak11@gmail.com>
2016-10-14 11:06:49 -0500

    Update BestPlayerMovement.cs
    ayy lmao

--- Assets/Scripts/BestPlayerMovement.cs
+++ Assets/Scripts/BestPlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class BestPlayerMovement : MonoBehaviour {
@@ -7,6 +7,7 @@ public class BestPlayerMovement : MonoBehaviour {
 	public Rigidbody rb;
 	private Camera cam;
 	public float speed;
+	public int angSpeed = 40;
 
 	void Start () {
 		rb = this.GetComponent<Rigidbody>();
@@ -15,19 +16,19 @@ public class BestPlayerMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float rot = Input.GetAxis("Horizontal");
 		float vSped = Input.GetAxis("Vertical");
-		rb.rotation = new Quaternion(1.0f,rot,1.0f,1.0f);
+		rb.transform.Rotate(0.0f, Input.GetAxis("Horizontal") * Time.deltaTime * angSpeed, 0.0f);
 		rb.AddRelativeForce(new Vector3(0.0f, 0.0f, vSped * speed));
-		if(Input.GetKeyDown(KeyCode.Space))
+		/*if(Input.GetKeyDown(KeyCode.Space))
 		{
 			waitTime(3);
 			rb.rotation = new Quaternion(1.0f,rot,1.0f,1.0f);
-		}
+		}*/
 	}
 	//Stabilization Func!
-	public IEnumerator waitTime(float waits){
+	/*public IEnumerator waitTime(float waits){
 		transform.rotation = Quaternion.identity;
 		yield return new WaitForSeconds(waits);
-	}
+	}*/
 }
+
