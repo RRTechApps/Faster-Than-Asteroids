b053d86c7e957b350f6d17028912494a9f403b03
jacobgreenly <jgreenly@swbell.net>
2016-10-21 11:54:03 -0500

    Add files via upload

---
+++ Assets/Scripts/BulletCollisionScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletCollisionScript : MonoBehaviour
+{
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "Bullet")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
