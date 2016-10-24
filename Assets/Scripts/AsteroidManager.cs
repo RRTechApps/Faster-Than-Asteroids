267b9a543bb6cf8e662cf682053742f0e3cc7a8e
Kenny Tang <kennyftang@gmail.com>
2016-10-24 16:17:35 -0500

    AsteroidManager works!

--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -13,21 +13,48 @@ public class AsteroidManager : MonoBehaviour {
 	public float asteroidLaunchVelocity = 20; 	//Magnitude of the launch velocity (will be vectorized in code)
 
 	private Vector3 fieldOffset;
-
+	private Vector3 fieldMargins;
+	private Vector3[] fieldCorners;
 
 	// Use this for initialization
 	void Start () {
 		fieldOffset = transform.position;
-
+		fieldMargins = new Vector3(1.5f, 0.0f, 1.5f);
+		fieldCorners = new Vector3[] {new Vector3(-fieldRadius.x, 0.0f, fieldRadius.z), new Vector3(fieldRadius.x, 0.0f, fieldRadius.z), new Vector3(-fieldRadius.x, 0.0f, -fieldRadius.z), new Vector3(fieldRadius.x, 0.0f, -fieldRadius.z)};
+		AddAsteroid();
 	}
-	
+
+	void Loop(){
+	}
+
+	//[Command]
+	//void CmdAddAsteroid(){
 	void AddAsteroid(){
-		//
-		float x, z; //x and/or z will be set to a corner value, the one not set to the corner value will have any value on the edge
-		Vector3 asteroidPosition = new Vector3(x, 0.0f, z) * fieldRadius + fieldOffset;
+		float x, z; 			//x and/or z will be set to a edge coordinate, the one not set to the edge coordinate will have any coordinate along the edge
 		Vector3 asteroidAngle;	//asteroidAngle will be any angle allowing the asteroid to travel through the field (bounds are 2 edges)
-		GameObject genAsteroid = Instantiate(asteroidPrefabs[Random.Range(0,3)], asteroidPosition, Quaternion.Euler(), this.transform);
-		//Vector3.Angle(genAsteroid.transform.position, 
-		//genAsteroid.GetComponent<Rigidbody>()
+		if(RandBool()) {		//Used to assign x and z to noted constraints 
+			x = Random.Range(-fieldRadius.x, fieldRadius.x);
+			z = RandBool() ? fieldRadius.z : -fieldRadius.z;
+		} else {
+			x = RandBool() ? fieldRadius.x : -fieldRadius.x;
+			z = Random.Range(-fieldRadius.z, fieldRadius.z);
+		}
+		//Point in the gamefield that we will transform.LookAt
+		Vector3 pointInField = new Vector3(Random.Range(-fieldRadius.x, fieldRadius.x), 0.0f, Random.Range(-fieldRadius.z, fieldRadius.z));
+		//Where the asteroid is
+		Vector3 asteroidPosition = new Vector3(x, 0.0f, z);
+		//Scale the asteroid to the margins
+		asteroidPosition.Scale(fieldMargins);
+		//Add on any offset from the origin
+		asteroidPosition += fieldOffset;
+		//Create the asteroid gameobject
+		GameObject genAsteroid = (GameObject)Instantiate(asteroidPrefabs[Random.Range(0,3)], asteroidPosition, Quaternion.Euler(Vector3.zero), this.transform);
+		//Point to the gamefield
+		genAsteroid.transform.LookAt(pointInField);
+		//Add a force to the asteroid to make it go through the field
+		genAsteroid.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * asteroidLaunchVelocity);
+	}
+	bool RandBool(){ 	//Returns true or false
+		return Random.Range(0, 2) == 0 ? true : false;
 	}
 }
