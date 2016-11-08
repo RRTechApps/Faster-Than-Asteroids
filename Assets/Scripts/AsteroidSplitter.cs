207b8f9ac41eb92d78ac2a2d410b2c366bea11c0
Zach Goldberg <highlowplays@gmail.com>
2016-11-08 11:51:19 -0600

    Memory Leak Plox Fix

--- Assets/Scripts/AsteroidSplitter.cs
+++ Assets/Scripts/AsteroidSplitter.cs
@@ -7,12 +7,15 @@ public class AsteroidSplitter : MonoBehaviour {
 	public float splitSpeed;
 
 	void OnTriggerEnter (Collider other) {
-		GameObject.Destroy (other.gameObject);
-		int splits = Random.Range (2, 4);
-		for (int x = 0; x < splits; x++) {
-			GameObject ast = (GameObject)Instantiate (asteroid, this.transform.position,this.transform.rotation);
-			ast.GetComponent<Rigidbody>().AddForce(new Vector3(splitSpeed*Random.Range(-1,2),0.0f,splitSpeed*Random.Range(-1,2)));
+		if(other.gameObject.tag =="Bullet")
+		{
+			GameObject.Destroy (other.gameObject);
+			int splits = Random.Range (2, 4);
+			for (int x = 0; x < splits; x++) {
+				GameObject ast = (GameObject)Instantiate (asteroid, this.transform.position,this.transform.rotation);
+				ast.GetComponent<Rigidbody>().AddForce(new Vector3(splitSpeed*Random.Range(-1,2),0.0f,splitSpeed*Random.Range(-1,2)));
+			}
+			GameObject.Destroy (this.gameObject);
 		}
-		GameObject.Destroy (this.gameObject);
 	}
 }
